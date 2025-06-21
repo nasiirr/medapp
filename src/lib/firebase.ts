514a94f1a873b26ca8e2a13b5899acf6a4442748
Mohd Nasir <mohdnasir80092@gmail.com>
2025-06-21 20:45:09 +0000

    dont change anything be it like it was

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,8 +6,8 @@ let app: FirebaseApp | null = null;
 let database: Database | null = null;
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_IDENTIFIER,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_HOST,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,

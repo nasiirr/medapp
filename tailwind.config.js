4be922976e78ea0829a481268f104b78452873b5
Mohd Nasir <mohdnasir80092@gmail.com>
2025-06-21 20:20:18 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
-import type {Config} from 'tailwindcss';
-
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   darkMode: ['class'],
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -103,4 +102,4 @@ export default {
     },
   },
   plugins: [require('tailwindcss-animate')],
-} satisfies Config;
+};

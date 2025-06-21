30f5dd92dcf29cc7b23048fc4b444172f397aadf
Mohd Nasir <mohdnasir80092@gmail.com>
2025-06-21 20:12:40 +0000

    remove all ai from code no use of ai in this project

--- src/ai/dev.ts
+++
@@ -1,8 +0,0 @@
-import { config } from 'dotenv';
-config();
-
-import '@/ai/flows/medication-log-component.ts';
-import '@/ai/flows/medication-schedule-edit-component.ts';
-import '@/ai/flows/schedule-display-component.ts';
-import '@/ai/flows/realtime-updates-flow.ts';
-import '@/ai/flows/firebase-setup.ts';
